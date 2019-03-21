ed0b2e4359e2ac6d1d6d6392c11edccfe4026cca
clockwerkz <clockwerkz@gmail.com>
2019-03-20 22:06:39 -0700

    first pass of css done - adds some button functionality

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,12 @@
 import '../scss/main.scss';
+import VanillaScrollSpy from 'vanillajs-scrollspy';
+
+const menu = document.getElementById('navbar');
+const scrollspy = new VanillaScrollSpy(menu, 3000);
+scrollspy.init();
+
+const menuIcon = document.querySelector('.navbar__icon');
+
+menuIcon.addEventListener('click', (e)=> {
+    menu.classList.toggle('show');
+});
