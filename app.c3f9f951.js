788b481a9385e5c38e7f35b57ef60ab7dd606783
clockwerkz <clockwerkz@gmail.com>
2019-03-25 00:52:48 -0700

    Updates

--- app.c3f9f951.js
+++ app.c3f9f951.js
@@ -388,7 +388,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58310" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56143" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
