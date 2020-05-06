debfa4665e52bda3ffffe44f92f7b3e332b4a56f
clockwerkz <clockwerkz@gmail.com>
2020-05-05 23:31:39 -0700

    refactoring slider code to factory pattern

--- src/js/carouselSlider.js
+++ src/js/carouselSlider.js
@@ -1,13 +1,62 @@
 import '../scss/main.scss';
 let timer = null;
-const lightImgs = document.querySelectorAll('.slideshow__img--light');
-const mobilePhone = document.querySelector('.mobile-phone');
 
 let slideIndex = 1;
 document.addEventListener("DOMContentLoaded", ()=> {
   showSlides(slideIndex);
-  //startTimer();
+  startTimer();
 })
+
+function createSlideshow(el) {
+  let slideIndex = 1;
+  let timer = null;
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  function showSlides(n) {
+    var i;
+    var slides = document.querySelectorAll(".mobile-phone .mySlides");
+    if (n > slides.length) {
+        slideIndex = 1
+      } else if (n < 1) {
+          slideIndex = slides.length
+      } else {
+          slideIndex = n;
+      }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+  }
+  
+  function startTimer() {
+    if (timer === null) {
+      timer = setInterval(function() {
+        showSlides(slideIndex+1);
+      }, 4000);
+    }
+  }
+  
+  function stopTimer() {
+    clearInterval(timer);
+    timer = null;
+  }
+}
 
 // Next/previous controls
 function plusSlides(n) {
@@ -21,7 +70,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
+  var slides = document.querySelectorAll(".mobile-phone .mySlides");
   if (n > slides.length) {
       slideIndex = 1
     } else if (n < 1) {
@@ -49,12 +98,12 @@ function stopTimer() {
 }
 
 
-document.querySelector('.next').addEventListener('click', function() {
+document.querySelector('.mobile-phone .next').addEventListener('click', function() {
   stopTimer();
   plusSlides(1);
   startTimer();
 });
-document.querySelector('.prev').addEventListener('click', function() {
+document.querySelector('.mobile-phone .prev').addEventListener('click', function() {
   stopTimer();
   plusSlides(-1);
   startTimer();
