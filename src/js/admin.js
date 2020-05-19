58bded2cd44d5c3533838040c29e57ffc71dda91
Carlos <clockwerkz@gmail.com>
2020-05-19 09:40:27 -0700

    adds first pass of select optio on admin page

--- src/js/admin.js
+++ src/js/admin.js
@@ -1,1 +1,12 @@
 import '../scss/main.scss';
+
+document.addEventListener("DOMContentLoaded", ()=> {
+
+    document.getElementById('portal-btn').addEventListener("click", (e)=>{
+        let selection = document.getElementById("portal-select").value;
+        console.log(selection);
+        if (selection) window.open(selection,'_blank');
+    });
+})
+
+
