3528e96b9033ea854da5578f34c02c04a11af8fd
clockwerkz <clockwerkz@gmail.com>
2020-05-24 18:13:45 -0700

    Updates

--- admin.522ed690.js
+++ admin.522ed690.js
@@ -229,7 +229,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57618" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50249" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -260,9 +260,8 @@ if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
         assetsToAccept.forEach(function (v) {
           hmrAcceptRun(v[0], v[1]);
         });
-      } else if (location.reload) {
-        // `location` global exists in a web worker context but lacks `.reload()` function.
-        location.reload();
+      } else {
+        window.location.reload();
       }
     }
 
