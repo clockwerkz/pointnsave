0c1cdfdb005a2e6496958292ded47325f9965dce
Carlos <clockwerkz@gmail.com>
2020-05-24 18:06:20 -0700

    Updates

--- app.c3f9f951.js
+++ app.c3f9f951.js
@@ -447,7 +447,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56611" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57618" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
