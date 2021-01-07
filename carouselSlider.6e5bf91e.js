b64b35276152bf2abf55c0a6c6e98cb76aa9dd68
Carlos <clockwerkz@gmail.com>
2021-01-06 20:09:55 -0800

    Updates

--- carouselSlider.6e5bf91e.js
+++ carouselSlider.6e5bf91e.js
@@ -437,7 +437,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50999" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60503" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
